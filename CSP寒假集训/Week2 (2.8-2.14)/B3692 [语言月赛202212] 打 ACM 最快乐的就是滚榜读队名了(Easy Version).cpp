978b6f1be3317a3889badb332c421ae04e06ac29
HK <3084318074@qq.com>
2026-02-13 22:25:53 +0800

    Create B3692 [语言月赛202212] 打 ACM 最快乐的就是滚榜读队名了(Easy Version).cpp

---
+++ CSP寒假集训/Week2 (2.8-2.14)/B3692 [语言月赛202212] 打 ACM 最快乐的就是滚榜读队名了(Easy Version).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TeamStatu{
+	string name;
+	int acs;
+	int times;
+	int ts;
+	bool q[100];
+}teams[1005];
+int main()
+{
+	//4小时之前（边输入边操作）：
+		//存入结构体
+	//4小时后（将输入存入数组）
+	//排序
+	//while循环，now=1
+		//遍历每道题
+			//如果ac了就排序一下，回到最初循环
+}
